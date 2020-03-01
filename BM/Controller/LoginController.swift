a29408e1e3545379328dd6997694e1205c2cc1fe
michaeladudarko <adudarkomichael@gmail.com>
2020-02-29 19:27:02 -0500

    Project Setup

--- BM/Controller/LoginController.swift
+++ BM/Controller/LoginController.swift
@@ -1,18 +1,20 @@
 //
-//  ViewController.swift
+//  LoginController.swift
 //  BM
 //
 //  Created by MICHAEL ADU DARKO on 2/29/20.
 //  Copyright © 2020 Bronzy. All rights reserved.
 //
 
+
 import UIKit
 
-class ViewController: UIViewController {
+class LoginController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = .red 
+    
     }
 
 

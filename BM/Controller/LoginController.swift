fc6f2db89b23d434fb73b090cf6479c92bf11e84
michaeladudarko <adudarkomichael@gmail.com>
2020-02-29 20:47:37 -0500

    UI label added

--- BM/Controller/LoginController.swift
+++ BM/Controller/LoginController.swift
@@ -11,12 +11,36 @@ import UIKit
 
 class LoginController: UIViewController {
 
+    // Mark: - Properties
+    
+    private let titleLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Black Market"
+        label.font = UIFont(name: "Avenir-Light", size: 36)
+        label.textColor = UIColor(white: 1, alpha: 0.9)
+        return label
+    }()
+    
+    
+    
+    // Mark: - Lifecycle
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red 
-    
+        view.backgroundColor = UIColor.init(red: 25/255, green: 25/255, blue: 25/255, alpha: 1)
+        
+        
+        view.addSubview(titleLabel)
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor,
+                                                   constant: 0).isActive = true
+ 
     }
 
-
+    override var preferredStatusBarStyle: UIStatusBarStyle{
+        return.lightContent
+    }
 }
 
